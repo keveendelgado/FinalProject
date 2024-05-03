d834da9d64ad321b9ebf1dbb347770a973e4bcde
keveendelgado <157523422+keveendelgado@users.noreply.github.com>
2024-05-03 15:16:20 -0400

    committ

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,18 @@ function scrollToTop() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 }
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
